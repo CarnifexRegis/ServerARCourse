e40ae28d362d04d4bdc5cc219faa987a545c3bc7
unknown <ge72xim@mytum.de>
2021-06-20 20:20:46 +0200

    fix now at least runs with no bugs (tried it with local db)

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 
 If you want to test install nodejs, visual studio code ide
 go into the terminal and type npm install (i think) to install all needed modules (they're git ignored)
+then do
+node "./src/server.js"
 Still didn't test it with local mongodb this is initial stuff 
--- src/server.js
+++ src/server.js
@@ -2,11 +2,11 @@ const express = require('express')
 const app = express()
 const path = require('path');
 const port = 3535;
-const url = "mongodb://localhost:27017/mydb"; //still needs fixing
+const url = "mongodb://localhost:27017/"; //still needs fixing
+const collectionName = "anchors";
+
 
 var mongoClient = require('mongodb').MongoClient;
-var queryManager = require('./queryManager');
-
 app.use(express.json());
 app.use(express.static('./client'));
 
@@ -21,7 +21,7 @@ app.get('/', (req, res) => {
  * DB INITIALISATION
  */
 
-mongoClient.connect(url, function (err, db) {
+mongoClient.connect(url, async function (err, db) {
     if (err) throw err;
     console.log("Database created!");
 
@@ -29,71 +29,93 @@ mongoClient.connect(url, function (err, db) {
     //we use this for db requests
     const dbo = db.db("guideDB");
 
-    //we create the anchors collection
-    dbo.createCollection("anchors", function (err, res) {
+    if (!collectionExists(dbo)) {
+        let { err, res } = await dbo.createCollection("anchors");
         if (err) throw err;
-        console.log("Collection created!");
+    }
 
-        const anchors = database.collection("anchors");
+    const anchors = dbo.collection(collectionName);
 
-        //all requests here
-        //gets an anchor by key?
-        app.get('/anchor', async (req, res) => {
-            try {
+    //all requests here
+    //gets an anchor by key?
+    app.get('/anchor', async (req, res) => {
+        try {
 
-                let query = req.body;
-                if (!query.hasOwnProperty('key')) {
-                    return res.status(400).send('Bad request');
-                }
-
-                let key = query.key;
-                const anchor = await anchors.findOne({ key }); // still don't know exactly functions this should return the anchor json
-
-                return res.status(200).send(anchor);
-
-
-
-            } catch (error) {
-                console.log(error);
-                return res.status(500).send('Server error');
+            let query = req.body;
+            if (!query.hasOwnProperty('key')) {
+                return res.status(400).send('Bad request');
             }
 
-        });
+            let key = query.key;
+            const anchor = await anchors.findOne({ key }); // still don't know exactly functions this should return the anchor json
 
-
-        //post a new anchor
-        app.post('/anchor', async (req, res) => {
-            try {
-
-                let query = req.query;
-                if (!query.hasOwnProperty('anchor')) {
-                    return res.status(400).send('Bad request');
-                }
-
-                let newAnchor = query.anchor;
-                const result = await anchors.insertOne(newAnchor);
-
-                console.log('New anchor was inserted with result ' + result);
+            return res.status(200).send(anchor);
 
 
 
+        } catch (error) {
+            console.log(error);
+            return res.status(500).send('Server error');
+        }
 
-            } catch (error) {
-                console.log(error);
-                return res.status(500).send('Server error');
+    });
+
+
+    //post a new anchor
+    app.post('/anchor', async (req, res) => {
+        try {
+
+            let query = req.query;
+            if (!query.hasOwnProperty('anchor')) {
+                return res.status(400).send('Bad request');
             }
 
-        });
+            let newAnchor = query.anchor;
+            const result = await anchors.insertOne(newAnchor);
 
-        //we try to detect shutting down and close client ocnnection
-        process.on('exit', function () {
-            console.log('About to exit.');
-            db.close();
-        }); 
+            console.log('New anchor was inserted with result ' + result);
+
+
+
+
+        } catch (error) {
+            console.log(error);
+            return res.status(500).send('Server error');
+        }
+
+    });
+
+    //we try to detect shutting down and close client ocnnection
+    process.on('exit', function () {
+        console.log('About to exit.');
+        db.close();
     });
 });
 
 
+/**
+ * Checks if the collection anchors exists
+ */
+async function collectionExists(dbo) {
+    let result = false;
+    let { err, collections } = dbo.listCollections().toArray();
+    if (err) throw err;
+
+    if (!collections) {
+        return;
+    }
+
+
+    for (var i = 0; i < collections.length; i++) {
+        if ((collections[i].name).localeCompare(collectionName) === 0) {
+            console.log((collections[i].name).localeCompare(collectionName));
+            result = true;
+            return;
+        }
+    }
+    return result;
+
+}
 
 
 
