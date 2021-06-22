8925095cebd46f4bdf923450acb6409b04a4177b
unknown <ge72xim@mytum.de>
2021-06-22 22:38:57 +0200

    client implementation

--- src/client/index.js
+++ src/client/index.js
@@ -1,54 +1,60 @@
 function indexScript(){
 
     //get request selectors
-    var getInput = $('#input-id');
-    var getResult = $('#get-result');
-    var getType = $('#select-search-type');
-    var getbtn = $('#getbtn');
+    this.$getInput = $('#input-id');
+    this.$getResult = $('#get-result');
+    this.$getType = $('#select-search-type');
+    this.$getbtn = $('#getbtn');
 
     //pôst request selectors
-    var postBody = $('#post-body');
-    var postResult = $('#post-result');
-    var postbtn = $('#postbtn');
+    this.$postBody = $('#post-body');
+    this.$postResult = $('#post-result');
+    this.$postbtn = $('#postbtn');
+
+    this.$getbtn.click((e) => this.sendGetRequest.call(this, this.$getType.val(), this.$getInput.val()));
+    this.$postbtn.click((e) => this.sendPstRequest.call(this, this.$postBody.val()));
 }
 
 
 
-indexScript.prototype.sendPostRequest = function(anchorjson){
+indexScript.prototype.sendGetRequest = function(searchParam, searchValue){
+    
+    let endpointurl = "anchor" + ((searchParam === "all")? "/all":"");
+
+    let Querydata = {};
+    Querydata[searchParam] = searchValue;
+    var me = this;
     $.ajax({
         type: "GET",
-        url: "anchor",
-        data: {
-            
-        },
+        url: endpointurl,
+        data: Querydata,
         Headers: {
             "content-type": "application/x-www-form-urlencoded"
         },
         success: function(data) {
-            location.reload()
+            me.$getResult.val(data);
         },
 
         error: function(res, statut, error){
-            loadingAnimation.disable(me.$inputs);
-            me.FailureFeedback(error);
+            me.$getResult.val(error);
         }
       });
 }
 
-indexScript.prototype.sendGetRequest = function(searchParam, searchtype){
+indexScript.prototype.sendPstRequest = function(anchorjson){
+    var me = this;
     $.ajax({
         type: "POST",
         url: "anchor",
-        data: JSON.stringify({
-
-        }),
+        data: JSON.stringify(anchorjson),
         contentType: "application/json",
         success: function(data) {
-
+            console.log(data);
+            me.$postResult.val(data);
         },
 
         error: function(res, statut, error){
-
+            me.$postResult.val(error);
         }
       });
 }
